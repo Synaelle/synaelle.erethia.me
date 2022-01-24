1517a4cdae5eec7d4d156a6bd290021f48b9f8cd
Synaelle <synaelle@protonmail.com>
2022-01-24 13:23:29 +0000

    hell

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function copyDiscord(){
+    if (!navigator.clipboard){
+        var discord = document.createElement("textarea");
+        document.body.appendChild(discord)
+        discord.value = "Synaelle#0001"
+        discord.select();
+        document.execCommand("copy");
+        document.body.removeChild(discord)
+    }
+    else{
+        navigator.clipboard.writeText(discord).then(
+            function(){
+                alert("Copied to clipboard!");
+            }   
+        ).catch(
+            function() {
+                alert("Error, can't copy to clipboard.");
+            }
+        ) 
+    }
+    
+}
+
+document.querySelector("#copy").addEventListener("click", copyDiscord);
